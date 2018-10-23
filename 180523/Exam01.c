1071e2c79693928dab12c8539b793f370c935366
Hyunwoo K <k_hyunwoo@icloud.com>
2018-10-23 09:33:44 +0900

    180523

---
+++ 180523/Exam01.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+	// 문자열 :  문자의 배열 상수
+	/*char str[100] = "Shim Yang-soo";
+	char str1[] = "Shim Yang-soo";
+	printf("%s\n", str);
+	for (int i = 0; str[i]; i++)
+		printf("%c", str[i]);*/
+
+	/*char s[100];
+	printf("너의 이름은 : ");
+	//scanf("%s", s);
+	fgets(stdin, sizeof(s), stdin);
+	printf("%s", s);*/
+
+	/*int a[2][3] = { 1, 2, 3, 4, 5 };
+	for (int i = 0; i < 2; i++){
+		for (int j = 0; j < 3; j++)
+			printf("%3d", a[i][j]);
+		puts(""); // 줄바꿈
+	}*/
+
+	/*int a[2][3] = { { 1, 2, 3 }, { 4, 5, 6 } };
+	for (int x = 0; x < 6; x++)
+		printf("%3d", ((int *)a)[x]);*/
+	
+
+}
